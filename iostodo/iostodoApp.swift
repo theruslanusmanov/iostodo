379def44c8fbcd1877ed9449f2bc8e261885087c
Ruslan Usmanov <ruslanusm6@gmail.com>
2023-07-02 21:44:19 +0300

    Initial Commit

---
+++ iostodo/iostodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iostodoApp.swift
+//  iostodo
+//
+//  Created by Ruslan Usmanov on 2023-07-02.
+//
+
+import SwiftUI
+
+@main
+struct iostodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
